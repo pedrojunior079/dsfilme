7daa0471be37d2c59bb5dfcbfe0d59dc8265bd9c
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-05-14 22:37:52 -0300

    MovieCard

--- frontend/src/pages/Listing.tsx
+++ frontend/src/pages/Listing.tsx
@@ -1,8 +1,41 @@
+import MovieCard from "components/MovieCard";
+
 import Pagination from "./Pagination";
 
-function Listing(){
-    return(
-        <Pagination/>
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                </div>
+            </div>
+        </>
     );
 }
 
